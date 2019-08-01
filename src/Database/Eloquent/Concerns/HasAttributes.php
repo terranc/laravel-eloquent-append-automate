199a9534a9e4d106eef2f62c2eb0423c472c0205
terranc <wuchaoboy@gmail.com>
2019-08-01 15:25:23 +0800

    Support for using append attributes as key names

--- src/Database/Eloquent/Concerns/HasAttributes.php
+++ src/Database/Eloquent/Concerns/HasAttributes.php
@@ -12,7 +12,7 @@ trait HasAttributes
             if (is_string($k)) {
                 $columns = explode('|', $k);
                 foreach ($columns as $column) {
-                    if (!isset($this->attributes[$column])) {
+                    if (!isset($this->$column)) {
                         unset($this->appends[$k]);
                     }
                 }
